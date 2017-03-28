barreiro/hibernate-orm

HHH-11459 - Test case

hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/merge/MergeEnhancedEntityTestTask.java
@@ -1,0 +1,97 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.bytecode.enhancement.merge;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.test.bytecode.enhancement.AbstractEnhancerTestTask;
+import org.junit.Assert;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Luis Barreiro
+ */
+public class MergeEnhancedEntityTestTask extends AbstractEnhancerTestTask {
+
+    private long entityId;
+
+    public Class<?>[] getAnnotatedClasses() {
+        return new Class<?>[]{Person.class, PersonAddress.class};
+    }
+
+    public void prepare() {
+        Configuration cfg = new Configuration();
+        cfg.setProperty( Environment.ENABLE_LAZY_LOAD_NO_TRANS, "true" );
+        cfg.setProperty( Environment.USE_SECOND_LEVEL_CACHE, "false" );
+        super.prepare( cfg );
+
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+
+            s.persist( new Person( 1L, "Sam" ) );
+            s.getTransaction().commit();
+        }
+    }
+
+    public void execute() {
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+            Person entity = s.find( Person.class, 1L );
+            entity.name = "Jhon";
+            try {
+                s.merge( entity );
+                s.getTransaction().commit();
+            } catch ( RuntimeException e ) {
+                Assert.fail( "Enhanced entity can't be refreshed: " + e.getMessage() );
+            }
+        }
+    }
+
+    protected void cleanup() {
+    }
+
+    @Entity
+    public static class Person {
+
+        @Id
+        private Long id;
+
+        @Column( name = "name", length = 10, nullable = false )
+        private String name;
+
+        @OneToMany( fetch = FetchType.LAZY, mappedBy = "parent", orphanRemoval = true, cascade = CascadeType.ALL )
+        private List<PersonAddress> details = new ArrayList<>();
+
+        protected Person() {
+        }
+
+        public Person(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Entity
+    public static class PersonAddress {
+
+        @Id
+        private Long id;
+
+        @ManyToOne( optional = false, fetch = FetchType.LAZY )
+        private Person parent;
+    }
+}

hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/merge/RefreshEnhancedEntityTestTask.java
@@ -1,0 +1,97 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.bytecode.enhancement.merge;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.test.bytecode.enhancement.AbstractEnhancerTestTask;
+import org.junit.Assert;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Luis Barreiro
+ */
+public class RefreshEnhancedEntityTestTask extends AbstractEnhancerTestTask {
+
+    private long entityId;
+
+    public Class<?>[] getAnnotatedClasses() {
+        return new Class<?>[]{Person.class, PersonAddress.class};
+    }
+
+    public void prepare() {
+        Configuration cfg = new Configuration();
+        cfg.setProperty( Environment.ENABLE_LAZY_LOAD_NO_TRANS, "true" );
+        cfg.setProperty( Environment.USE_SECOND_LEVEL_CACHE, "false" );
+        super.prepare( cfg );
+
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+
+            s.persist( new Person( 1L, "Sam" ) );
+            s.getTransaction().commit();
+        }
+    }
+
+    public void execute() {
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+            Person entity = s.find( Person.class, 1L );
+            entity.name = "Jhon";
+            try {
+                s.refresh( entity );
+                s.getTransaction().commit();
+            } catch ( RuntimeException e ) {
+                Assert.fail( "Enhanced entity can't be refreshed: " + e.getMessage() );
+            }
+        }
+    }
+
+    protected void cleanup() {
+    }
+
+    @Entity
+    public static class Person {
+
+        @Id
+        private Long id;
+
+        @Column( name = "name", length = 10, nullable = false )
+        private String name;
+
+        @OneToMany( fetch = FetchType.LAZY, mappedBy = "parent", orphanRemoval = true, cascade = CascadeType.ALL )
+        private List<PersonAddress> details = new ArrayList<>();
+
+        protected Person() {
+        }
+
+        public Person(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Entity
+    public static class PersonAddress {
+
+        @Id
+        private Long id;
+
+        @ManyToOne( optional = false, fetch = FetchType.LAZY )
+        private Person parent;
+    }
+}

gbadner/hibernate-core

(cherry picked from commit 2779ebd8f0e8f36cc8ba30757c9a3ec318c6a5b1)
HHH-11459 : Correct test case to work on pre-5.2 versions
HHH-11459 - Do not fetch a collection on cascade, resolve it instead
(cherry picked from commit 41bd06266d3e64268fb49f003b24072c2fe685c2)
HHH-11459 - Always resolve unloaded, lazy collection in enhanced entity when cascading; added comments
(cherry picked from commit 0b6ce73fcc6c5bba34e0221395c15b66e06b2a36)
HHH-11459 : Add entity names to avoid Oracle test failures
(cherry picked from commit 28f3148f7b11581aba557b971d2d5ef809a2f30d)
HHH-11459 : Correct fix to work on pre-5.2 versions
(cherry picked from commit ba86f1037862debfdb28d4a380dd28d2f99157bd)
Conflicts:
	hibernate-core/src/main/java/org/hibernate/engine/internal/Cascade.java
	hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/EnhancerTest.java

hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/merge/MergeEnhancedEntityTestTask.java
@@ -1,0 +1,97 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.bytecode.enhancement.merge;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.test.bytecode.enhancement.AbstractEnhancerTestTask;
+import org.junit.Assert;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Luis Barreiro
+ */
+public class MergeEnhancedEntityTestTask extends AbstractEnhancerTestTask {
+
+    private long entityId;
+
+    public Class<?>[] getAnnotatedClasses() {
+        return new Class<?>[]{Person.class, PersonAddress.class};
+    }
+
+    public void prepare() {
+        Configuration cfg = new Configuration();
+        cfg.setProperty( Environment.ENABLE_LAZY_LOAD_NO_TRANS, "true" );
+        cfg.setProperty( Environment.USE_SECOND_LEVEL_CACHE, "false" );
+        super.prepare( cfg );
+
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+
+            s.persist( new Person( 1L, "Sam" ) );
+            s.getTransaction().commit();
+        }
+    }
+
+    public void execute() {
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+            Person entity = s.get( Person.class, 1L );
+            entity.name = "Jhon";
+            try {
+                s.merge( entity );
+                s.getTransaction().commit();
+            } catch ( RuntimeException e ) {
+                Assert.fail( "Enhanced entity can't be refreshed: " + e.getMessage() );
+            }
+        }
+    }
+
+    protected void cleanup() {
+    }
+
+    @Entity( name = "Person" )
+    public static class Person {
+
+        @Id
+        private Long id;
+
+        @Column( name = "name", length = 10, nullable = false )
+        private String name;
+
+        @OneToMany( fetch = FetchType.LAZY, mappedBy = "parent", orphanRemoval = true, cascade = CascadeType.ALL )
+        private List<PersonAddress> details = new ArrayList<>();
+
+        protected Person() {
+        }
+
+        public Person(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Entity( name = "PersonAddress" )
+    public static class PersonAddress {
+
+        @Id
+        private Long id;
+
+        @ManyToOne( optional = false, fetch = FetchType.LAZY )
+        private Person parent;
+    }
+}

hibernate-core/src/test/java/org/hibernate/test/bytecode/enhancement/merge/RefreshEnhancedEntityTestTask.java
@@ -1,0 +1,97 @@
+/*
+ * Hibernate, Relational Persistence for Idiomatic Java
+ *
+ * License: GNU Lesser General Public License (LGPL), version 2.1 or later.
+ * See the lgpl.txt file in the root directory or <http://www.gnu.org/licenses/lgpl-2.1.html>.
+ */
+package org.hibernate.test.bytecode.enhancement.merge;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.Environment;
+import org.hibernate.test.bytecode.enhancement.AbstractEnhancerTestTask;
+import org.junit.Assert;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author Luis Barreiro
+ */
+public class RefreshEnhancedEntityTestTask extends AbstractEnhancerTestTask {
+
+    private long entityId;
+
+    public Class<?>[] getAnnotatedClasses() {
+        return new Class<?>[]{Person.class, PersonAddress.class};
+    }
+
+    public void prepare() {
+        Configuration cfg = new Configuration();
+        cfg.setProperty( Environment.ENABLE_LAZY_LOAD_NO_TRANS, "true" );
+        cfg.setProperty( Environment.USE_SECOND_LEVEL_CACHE, "false" );
+        super.prepare( cfg );
+
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+
+            s.persist( new Person( 1L, "Sam" ) );
+            s.getTransaction().commit();
+        }
+    }
+
+    public void execute() {
+        try ( Session s = getFactory().openSession() ) {
+            s.beginTransaction();
+            Person entity = s.get( Person.class, 1L );
+            entity.name = "Jhon";
+            try {
+                s.refresh( entity );
+                s.getTransaction().commit();
+            } catch ( RuntimeException e ) {
+                Assert.fail( "Enhanced entity can't be refreshed: " + e.getMessage() );
+            }
+        }
+    }
+
+    protected void cleanup() {
+    }
+
+    @Entity(name = "Person")
+    public static class Person {
+
+        @Id
+        private Long id;
+
+        @Column( name = "name", length = 10, nullable = false )
+        private String name;
+
+        @OneToMany( fetch = FetchType.LAZY, mappedBy = "parent", orphanRemoval = true, cascade = CascadeType.ALL )
+        private List<PersonAddress> details = new ArrayList<>();
+
+        protected Person() {
+        }
+
+        public Person(Long id, String name) {
+            this.id = id;
+            this.name = name;
+        }
+    }
+
+    @Entity(name = "PersonAddress")
+    public static class PersonAddress {
+
+        @Id
+        private Long id;
+
+        @ManyToOne( optional = false, fetch = FetchType.LAZY )
+        private Person parent;
+    }
+}